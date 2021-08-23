9ae820cb26ca6fefabff1ca5ff2ec19fb92bf5f1
Lokesh <72940205+Lokesh-Amara@users.noreply.github.com>
2021-08-23 10:54:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(function () {
+  document.getElementById("pp").innerText = "10";
+  setTimeout(function () {
+    document.getElementById("pp").innerText = "9";
+    setTimeout(function () {
+      document.getElementById("pp").innerText = "8";
+      setTimeout(function () {
+        document.getElementById("pp").innerText = "7";
+        setTimeout(function () {
+          document.getElementById("pp").innerText = "6";
+          setTimeout(function () {
+            document.getElementById("pp").innerText = "5";
+            setTimeout(function () {
+              document.getElementById("pp").innerText = "4";
+              setTimeout(function () {
+                document.getElementById("pp").innerText = "3";
+                setTimeout(function () {
+                  document.getElementById("pp").innerText = "2";
+                  setTimeout(function () {
+                    document.getElementById("pp").innerText = "1";
+                    setTimeout(function () {
+                      document.getElementById("pp").innerText =
+                        "Happy Independence Day...";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
